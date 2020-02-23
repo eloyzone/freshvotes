e018df4b405b730527d661f8a6851002bc8f9a02
Elyas Hadizadeh Tasbiti <elyas_hadizadeh@hotmail.com>
2020-02-23 19:33:31 +0330

    Initial commit: Basic structure for a SpringBoot project.

---
+++ src/main/java/com/github/eloyzone/freshvotes/FreshvotesApplication.java
@@ -0,0 +1,15 @@
+package com.github.eloyzone.freshvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshvotesApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FreshvotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/eloyzone/freshvotes/FreshvotesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.github.eloyzone.freshvotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshvotesApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
