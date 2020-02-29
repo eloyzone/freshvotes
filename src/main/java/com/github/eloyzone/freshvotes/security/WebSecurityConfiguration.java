4995088ea57482a385e0e2a49ab62fc814ba1b89
Elyas Hadizadeh Tasbiti <elyas_hadizadeh@hotmail.com>
2020-02-29 12:31:27 +0330

    Login page tweaked and Password-Encoder added.

--- src/main/java/com/github/eloyzone/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/github/eloyzone/freshvotes/security/WebSecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.github.eloyzone.freshvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author Eloy 'Elyas' Hadizadeh Tasbiti
@@ -13,18 +15,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
 {
+    @Bean
+    public PasswordEncoder getPasswordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception
     {
         authenticationManagerBuilder.inMemoryAuthentication().
-                withUser("eloy").password("123").roles("USER");
+                passwordEncoder(getPasswordEncoder()).
+                withUser("eloy").password(getPasswordEncoder().encode("123")).roles("USER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        http.csrf().disable().
-                authorizeRequests().antMatchers("/").permitAll().
+        http.csrf().disable().authorizeRequests().
+                antMatchers("/").permitAll().
                 anyRequest().hasRole("USER").and().
                 formLogin().loginPage("/login").permitAll().and().
                 logout().logoutUrl("/logout").permitAll();
